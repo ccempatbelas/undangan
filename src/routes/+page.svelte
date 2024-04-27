3876e51e87754a30a541121cd28008a612889dd8
lesmanadel <lesmanadel3@gmail.com>
2024-04-28 03:51:02 +0700

    remove unused on main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import { audio, cover, muted } from '@/lib/stores';
   import { inview } from 'svelte-inview';
   import Section from '@/lib/components/layouts/Section.svelte';
-  import { Volume2, CalendarPlus, Image, VolumeX, ArrowLeft, ArrowRight } from 'lucide-svelte';
+  import { CalendarPlus, Image, ArrowLeft, ArrowRight } from 'lucide-svelte';
   import { fade } from 'svelte/transition';
   import { superForm } from 'sveltekit-superforms/client';
-  import ShootingStar from '@/lib/components/ShootingStar.svelte';
   import {
     getModalStore,
     type ModalSettings,
@@ -25,12 +23,6 @@
   const { form: guest } = superForm(data.guest, {
     dataType: 'json'
   });
-
-  // Days remaining
-  const daysRemaining = Math.ceil(
-    (new Date(new Date().getFullYear(), 7, 27).getTime() - new Date().getTime()) /
-      (1000 * 60 * 60 * 24)
-  );
 
   // Gallery Images
   const allImages = Object.keys(import.meta.glob('static/images/placeholder/*')).map((p) =>
@@ -55,19 +47,10 @@
     modalStore.trigger(modal);
   }
 
-  let visible = false;
-
   // Anmimation
   let animOptions = {
     rootMargin: '-100px',
     unobserveOnEnter: true
-  };
-
-  let inviewFlag = {
-    holyMat: false,
-    reception: false,
-    rsvp: false,
-    gift: false
   };
 
   let elemGallery: HTMLDivElement;
@@ -99,19 +82,9 @@
   <meta name="the description" content="Svelte demo app" />
 </svelte:head>
 
-<svelte:window
-  on:scroll={() => {
-    if (window.scrollY > window.innerHeight / 8) {
-      visible = true;
-    } else {
-      visible = false;
-    }
-  }}
-/>
 
 <!-- For Eager loading -->
-<img src={img} alt="invisible" hidden />
-<div class="bg-nebula relative flex justify-center overflow-hidden">
+<div class="relative flex justify-center overflow-hidden">
   <div class="max-w-5xl">
     <header id="head" class="flex justify-center" transition:fade={{ duration: 3000 }}>
       <div
@@ -176,8 +149,8 @@
         <div
           class="flex opacity-0"
           use:inview={animOptions}
-          class:animRight={inviewFlag.holyMat}
-          on:inview_change={({ detail }) => (inviewFlag.holyMat = detail.inView)}
+          class:animRight={true}
+          on:inview_change={({ detail }) => (true)}
         >
           <div class="relative mr-4 basis-3/5 text-right xs:basis-1/2">
             <p>
@@ -301,11 +274,11 @@
       <div
         class="mb-64 mt-8 flex justify-center object-none object-bottom opacity-0"
         use:inview={{ unobserveOnEnter: true }}
-        class:animFade={inviewFlag.rsvp}
-        on:inview_change={({ detail }) => (inviewFlag.rsvp = detail.inView)}
+        class:animFade={true}
+        on:inview_change={({ detail }) => (itrue)}
       >
         <div class="flex max-h-full w-full max-w-lg flex-col justify-center gap-8">
-          <p class="text-center">More information coming soon!</p>
+          <p class="text-center">RSVP dibuka 14 Mei!</p>
           <div class="relative">
             <div
               class="gradient-heading animScale rounded-lg border border-primary-200 from-primary-500 to-primary-50 px-8 py-4 text-3xl shadow-primary-300 text-shadow-sm md:text-5xl"
@@ -317,7 +290,7 @@
       </div>
     </Section>
     <p class="mb-20 text-center text-sm text-primary-200 [&>*]:pointer-events-none">
-      still building more exciting stuff here
+      Mari ketemuaannnn
     </p>
   </div>
 </div>
