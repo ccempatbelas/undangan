5bb65f50328e2a2785f9c417067c85e34366e9f6
lesmanadel <lesmanadel3@gmail.com>
2024-04-27 23:57:56 +0700

    bersihin /sponsor

--- src/routes/sponsor/+page.svelte
+++ src/routes/sponsor/+page.svelte
@@ -18,44 +18,7 @@
   import Nav from '@/lib/components/Nav.svelte';
   import Cover from '@/lib/components/Cover.svelte';
 
-  // Load Data
-  export let data;
-  const modalStore = getModalStore();
 
-  const { form: guest } = superForm(data.guest, {
-    dataType: 'json'
-  });
-
-  // Days remaining
-  const daysRemaining = Math.ceil(
-    (new Date(new Date().getFullYear(), 7, 27).getTime() - new Date().getTime()) /
-      (1000 * 60 * 60 * 24)
-  );
-
-  // Gallery Images
-  const allImages = Object.keys(import.meta.glob('static/images/placeholder/*')).map((p) =>
-    p.replace('/static', '')
-  );
-  let imagePaths = allImages;
-  let img = '';
-  onMount(() => {
-    if ($guest.id) {
-      localStorage.setItem('code', $guest.id);
-    }
-    img = '/images/cover.jpg';
-  });
-
-  function openImage(i: number): void {
-    const c: ModalComponent = { ref: ImageModal };
-    const modal: ModalSettings = {
-      type: 'component',
-      component: c,
-      meta: { images: imagePaths, index: i }
-    };
-    modalStore.trigger(modal);
-  }
-
-  let visible = false;
 
   // Anmimation
   let animOptions = {
@@ -63,35 +26,6 @@
     unobserveOnEnter: true
   };
 
-  let inviewFlag = {
-    holyMat: false,
-    reception: false,
-    rsvp: false,
-    gift: false
-  };
-
-  let elemGallery: HTMLDivElement;
-
-  function multiColumnLeft(): void {
-    let x = elemGallery.scrollWidth;
-    if (elemGallery.scrollLeft !== 0) x = elemGallery.scrollLeft - elemGallery.clientWidth;
-    elemGallery.scroll(x, 0);
-  }
-
-  function multiColumnRight(): void {
-    let x = 0;
-    // -1 is used because different browsers use different methods to round scrollWidth pixels.
-    if (elemGallery.scrollLeft < elemGallery.scrollWidth - elemGallery.clientWidth - 1)
-      x = elemGallery.scrollLeft + elemGallery.clientWidth;
-    elemGallery.scroll(x, 0);
-  }
-
-  // popup
-  const popupHover: PopupSettings = {
-    event: 'hover',
-    target: 'popupHover',
-    placement: 'top'
-  };
 </script>
 
 <svelte:head>
@@ -110,7 +44,6 @@
 />
 
 <!-- For Eager loading -->
-<img src={img} alt="invisible" hidden />
 <div class="bg-nebula relative flex justify-center overflow-hidden">
   <div class="max-w-5xl">
     <header id="head" class="flex justify-center" transition:fade={{ duration: 3000 }}>
@@ -125,8 +58,8 @@
         <div
           class="mb-64 mt-8 flex justify-center object-none object-bottom opacity-0"
           use:inview={{ unobserveOnEnter: true }}
-          class:animFade={inviewFlag.rsvp}
-          on:inview_change={({ detail }) => (inviewFlag.rsvp = detail.inView)}
+          class:animFade={true}
+          on:inview_change={({ detail }) => (true)}
         >
           <div class="flex max-h-full w-full max-w-lg flex-col justify-center gap-8">
             <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/locahouse.id/" class="flex w-full flex-col items-center justify-center"
@@ -143,10 +76,13 @@
           </div>
         </div>
         <p class="text-center">
-          Want to support this event? Contact <a
+          Want to support this event? Check <a
+              class="text-primary-300 underline"
+              href="/backers">here</a
+            > or contact <a
             class="text-primary-300 underline"
             target="_blank"
-            href="https://wa.me/628999966385">here</a
+            href="https://wa.me/628999966385">DL</a
           > :)
         </p>
       </Section>
