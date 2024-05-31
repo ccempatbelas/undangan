6560d623a617f285852b204b1d099164aaba0656
lesmanadel <lesmanadel3@gmail.com>
2024-05-31 21:59:19 +0700

    fix selected on navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -14,19 +14,19 @@
     border="border-token"
     class="bg-surface-100-800-token w-full"
   >
-    <TabAnchor  href="/" selected={$page.url.pathname === '/'}>
+    <TabAnchor  href="/" selected={$page.url.pathname === '' || $page.url.pathname ===  '/'}>
       <div class="flex items-center justify-center"><Home /></div>
       <div><span>Home</span></div>
     </TabAnchor>
-    <TabAnchor href="/rsvp" selected={$page.url.pathname === '/rsvp'}>
+    <TabAnchor href="/rsvp" selected={$page.url.pathname === '/rsvp' || $page.url.pathname ===  '/rsvp/'}>
       <div class="flex items-center justify-center"><PencilLine /></div>
       <div><span>RSVP</span></div>
     </TabAnchor>
-    <TabAnchor href="/backers" selected={$page.url.pathname === '/backers'}>
+    <TabAnchor href="/backers" selected={$page.url.pathname === '/backers' || $page.url.pathname ===  '/backers/'}>
       <div class="flex items-center justify-center"><Users /></div>
       <div><span>Backers</span></div>
     </TabAnchor>
-    <TabAnchor href="/become-backers"  selected={$page.url.pathname === '/become-backers'}>
+    <TabAnchor href="/become-backers"  selected={$page.url.pathname === '/become-backers' || $page.url.pathname === '/become-backers/'}>
       <div class="flex items-center justify-center"><Coins /></div>
       <div><span>Support Us</span></div>
     </TabAnchor>
