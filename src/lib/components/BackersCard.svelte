ce7b84f417253f8421b593e7953b26bacf6553a9
lesmanadel <lesmanadel3@gmail.com>
2024-06-01 00:45:04 +0700

    refactor backer, add more sponsor

---
+++ src/lib/components/BackersCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let url: string;
+  export let image: string;
+  export let desc: string;
+</script>
+
+<a
+  target="_blank"
+  rel="noopener noreferrer"
+  href="{url}"
+  class="flex w-full flex-col items-center justify-center"
+  >
+  <div class="relative border border-primary-200 shadow-primary-300">
+    <img src="/images/logo-backers/{image}.jpg" alt="{image}" />
+  </div>
+  <div><span>{desc}</span></div>
+</a>
--- src/routes/backers/+page.svelte
+++ src/routes/backers/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Section from '@/lib/components/layouts/Section.svelte';
   import NavBar from '@/lib/components/NavBar.svelte';
+  import BackersCard from '@/lib/components/BackersCard.svelte';
 </script>
 
 <svelte:head>
@@ -22,57 +23,36 @@
     <Section id="rsvp" title="This event is supported by" class="h4 text-secondary-500">
       <div class="mb-64 mt-8 flex justify-center object-none object-bottom">
         <div class="flex max-h-full w-full max-w-lg flex-col justify-center gap-4" style="font-size: {14 + 'px'}">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="hhttps://linktr.ee/colonytackle"
-            class="flex w-full flex-col items-center justify-center"
-            ><div class="relative border border-primary-200 shadow-primary-300">
-              <img src="/images/logo-backers/logo-colony-tackle.jpg" alt="logo-colony-tackle" />
-            </div>
-            <div>Distributor Fishing Hook | Adhe</div>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://linktr.ee/DuniaAuto"
-            class="flex w-full flex-col items-center justify-center"
-            ><div class="relative border border-primary-200 shadow-primary-300">
-              <img src="/images/logo-backers/logo-duniaauto.jpg" alt="logo-duniaauto" />
-            </div>
-            <div>Aksesoris mobil, emblem, stiker, dll. | Reinard</div>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.halim-sakti.com/products"
-            class="flex w-full flex-col items-center justify-center"
-            ><div class="relative border border-primary-200 shadow-primary-300">
-              <img src="/images/logo-backers/logo-halim-sakti.jpg" alt="logo-halim-sakti" />
-            </div>
-            <div>Speciality and industrial chemicals supplier | Bampi</div>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.instagram.com/locahouse.id/"
-            class="flex w-full flex-col items-center justify-center"
-            
-            ><div class="relative border border-primary-200 shadow-primary-300" >
-              <img src="/images/logo-backers/logo-loca-house.png" alt="loca-house" />
-            </div>
-            <div>Cafe, Resto, Catering, Private Event | Yosta</div>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.vidio.com/categories/movies"
-            class="flex w-full flex-col items-center justify-center"
-            ><div class="relative border border-primary-200 shadow-primary-300">
-              <img src="/images/logo-backers/logo-dl.jpg" alt="logo-dl" />
-            </div>
-            <div>Nonton apa aja di Vidio | DL</div>
-          </a>
+          <BackersCard
+            url="https://linktr.ee/colonytackle"
+            image="logo-colony-tackle"
+            desc="Distributor Fishing Hook | Adhe"
+          />
+          <BackersCard
+            url="https://linktr.ee/DuniaAuto"
+            image="logo-duniaauto"
+            desc="Aksesoris mobil, emblem, stiker, dll. | Reinard"
+          />
+          <BackersCard
+            url="https://www.halim-sakti.com/products"
+            image="logo-halim-sakti"
+            desc="Speciality and industrial chemicals supplier | Bampi"
+          />
+          <BackersCard
+            url="https://g.co/kgs/QBSvL3K"
+            image="logo-uma-sukalila"
+            desc="Hotel UMA Sukalila | Willy"
+          />
+          <BackersCard
+            url="https://linktr.ee/locahouse.id"
+            image="logo-loca-house"
+            desc="Cafe, Resto, Catering, Private Event | Yosta"
+          />
+          <BackersCard
+            url="https://www.vidio.com/categories/movies"
+            image="logo-dl"
+            desc="Nonton apa aja di Vidio | DL"
+          />
         </div>
       </div>
       <p class="mb-20 text-center text-sm">
