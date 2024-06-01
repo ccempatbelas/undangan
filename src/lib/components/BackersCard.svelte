9ef75c590ea438e04cb22bc763701cce7bfec972
lesmanadel <lesmanadel3@gmail.com>
2024-06-01 14:44:09 +0700

    change CardBackers border style

--- src/lib/components/BackersCard.svelte
+++ src/lib/components/BackersCard.svelte
@@ -4,13 +4,21 @@
   export let desc: string;
 </script>
 
+<style>
+.card-border {
+border-image: linear-gradient(117deg, #ffb36b, #6d87de) 30;
+border-width: 5px;
+border-style: solid;
+}
+</style>
+
 <a
   target="_blank"
   rel="noopener noreferrer"
   href="{url}"
   class="flex w-full flex-col items-center justify-center"
   >
-  <div class="relative border border-primary-200 shadow-primary-300">
+  <div class="relative border card-border">
     <img src="/images/logo-backers/{image}.jpg" alt="{image}" />
   </div>
   <div><span>{desc}</span></div>
